0c407b08089584c2827c0c49acb255b1c324ad8f
Lisinchyk <lisinchyk@gmail.com>
2020-02-16 03:07:38 +0200

    Uppload test task

---
+++ src/js/scripts.js
@@ -0,0 +1,26 @@
+const menuBurger = document.querySelector('.nav__burger');
+
+menuBurger.addEventListener('click', function() {
+    const navMenuMobile = document.querySelector('.nav__menu-mobile');
+    menuBurger.classList.toggle('active');
+    navMenuMobile.classList.toggle('active');
+});
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        // responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            700: {
+                items: 2
+            },
+            900: {
+                items: 4
+            }
+        }
+    });
+});
